1645e1a72cb34655a9e7dad658788a1f428ca80d
Ian <ian0731@gmail.com>
2022-08-13 16:11:30 +0800

    first version

---
+++ ToDoListAsync/Controllers/TodoController.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ToDoListAsync.Models;
+
+namespace ToDoListAsync.Controllers
+{
+    public class TodoController : Controller
+    {
+        private readonly ToDoListContext _context;
+
+        public TodoController(ToDoListContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var item = from i in _context.ToDoListTables 
+                                select i;
+
+            if(item == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return View(await item.ToListAsync());
+            }   
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(ToDoListTable _toDoListTable)
+        {
+            if ((_toDoListTable != null) && (ModelState.IsValid))
+            {
+                _context.ToDoListTables.Add(_toDoListTable);
+                await _context.SaveChangesAsync();
+
+                TempData["Success"] = "新增成功";
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                TempData["Error"] = "新增失敗";
+            }
+            return View();
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new StatusCodeResult((int)System.Net.HttpStatusCode.BadRequest);
+            }
+
+            ToDoListTable item = _context.ToDoListTables.Find(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return View(item);
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(ToDoListTable toDoListTable)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.ToDoListTables.Update(toDoListTable);
+                await _context.SaveChangesAsync();
+
+                TempData["Success"] = "修改成功";
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                TempData["Error"] = "修改失敗";
+            }
+            return View();
+        }
+        public async Task<IActionResult> Delete(int id)
+        {
+            ToDoListTable item = await _context.ToDoListTables.FindAsync(id);
+
+            if (item == null)
+            {
+                TempData["Error"] = "此筆資料不存在";
+            }
+            else
+            {
+                _context.ToDoListTables.Remove(item);
+                await _context.SaveChangesAsync();
+
+                TempData["Success"] = "刪除成功";
+            }
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ ToDoListAsync/Models/ToDoListTable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace ToDoListAsync.Models
+{
+    public partial class ToDoListTable
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [Display(Name ="待辦事項")]
+        public string Task { get; set; } = null!;
+    }
+}
